881484dd86c289180a33d489d7b0335890f81f0a
drago <dragoljub.qa@gmail.com>
2022-12-07 00:48:29 +0100

    //

---
+++ src/Zadatak47.java
@@ -0,0 +1,15 @@
+public class Zadatak47 {
+    public static void main(String[] args) {
+
+        //Zadatak 47) U void metodi kreirati niz, u main metodi prikazati 3. clan tog niza
+
+        stampanjeNiza(2);
+
+    }
+
+    public static void stampanjeNiza(int x) {
+        int[] niz = {1,5,9,11,13};
+        System.out.println(niz[x]);
+
+    }
+}
